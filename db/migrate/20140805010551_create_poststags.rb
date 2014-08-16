1f09ab11361fb674bab3a22937998ce460d41f7b
jmscholen <jmscholen@gmail.com>
2014-08-16 16:50:22 -0400

    dropping resume and prepping for blog

---
+++ db/migrate/20140805010551_create_poststags.rb
@@ -0,0 +1,10 @@
+class CreatePoststags < ActiveRecord::Migration
+  def change
+    create_table :posts_tags, id: false do |t|
+      t.integer :post_id
+      t.integer :tag_id
+
+      t.timestamps
+    end
+  end
+end
