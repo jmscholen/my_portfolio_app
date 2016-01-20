af79288cb9ef254bc36112957fd563acb47dc8d4
Jeff <jmscholen@gmail.com>
2016-01-20 10:48:59 -0500

    updating info

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,8 +1,19 @@
 class ResumeController < ApplicationController
-	layout "application", except: [:index]
+	# layout "application", except: [:index]
+
 	def index
+    # @download_resume = download_resume
 	end
 
 	def show
 	end
+
+  # def download_resume
+  #   pdf = WickedPdf.new.pdf_from_html_file('app/views/resume/myresume.html')
+  #   save_path = Rails.root.join('pdfs','filename.pdf')
+  #   File.open(save_path, 'wb') do |file|
+  #     file << pdf
+  #   end
+  #   send_file file.path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :resume
+
   resources :welcome
   resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
